f318810275e10ab0b1999820cbc95f284d26d732
Bestbatazaaa <Bestbatazaaa@icloud.com>
2023-12-01 13:54:10 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,17 @@
 using namespace std;
 
 int main(){
+    int num, odd = 0, even = -1;
+    while ( num != 0) { 
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> num;
+    if (num%2 == 0){
+        even = even + 1;
+    } else {
+        odd = odd + 1;
+    }
+    }
+    cout << "#Even numbers = " << even;
+    cout << "\n#Odd numbers = " << odd;
     return 0;
 }
